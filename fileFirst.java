aa8aa46ff07b5e9bc1ddae8db2573d24985cc8f7
sonali <sonali@nagarro.com>
2023-09-25 11:46:14 +0530

    first commit

---
+++ fileFirst.java
@@ -0,0 +1,23 @@
+class myOneClass{
+
+ 
+
+   public static void main(String args[])  //static method  
+
+    {  
+
+        Sub();
+
+    }  
+
+ 
+
+    public static void Sub(){
+
+        System.out.println(4-2);
+
+    }
+
+ 
+
+}
